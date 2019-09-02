13a7831c1260e244d727e58d7097681d2d4a9ae1
Leonov Andrew <leonov_andrew@mail.ru>
2019-09-02 07:26:19 +0300

    all 1

---
+++ pages.php
@@ -0,0 +1,164 @@
+<?php include_once __DIR__."/header.php";?>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2>
+                    Страницы
+                </h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <ul>
+                    <li>
+                        <a href="/">
+                            Главная
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/about.php">
+                            О компании
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/cart.php">
+                            Корзина
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/carta.php">
+                            Подарочная карта
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/catalog.php">
+                            Каталог
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/category.php">
+                            Категории
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/contact.php">
+                            Контакты
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/contact_detal.php">
+                            Контакты детальная
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/detal.php">
+                            Детальная продукты
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/dostavka.php">
+                            Доставка
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/izbran.php">
+                            Избранное
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/lk_data.php">
+                            Личный кабинет 1
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/lk_data_dost.php">
+                            Личный кабинет 2
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/lk_email.php">
+                            Личный кабинет 3
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/lk_fav.php">
+                            Личный кабинет 4
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/lk_history.php">
+                            Личный кабинет 5
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/lk_pwd.php">
+                            Личный кабинет 6
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/news.php">
+                            Новости
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/opt.php">
+                            Оптовикам
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/order1.php">
+                            Заказы 1
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/order2.php">
+                            Заказы 2
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/order2-1.php">
+                            Заказы 2-1
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/order3.php">
+                            Заказы 3
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/order_final.php">
+                            Заказы 4
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/registr.php">
+                            Регистрация
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/registr_1.php">
+                            Регистрация 2
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/registr_2.php">
+                            Регистрация 3
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/registr_3.php">
+                            Регистрация 4
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/srav.php">
+                            Сравнение
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include_once __DIR__."/footer.php";?>
