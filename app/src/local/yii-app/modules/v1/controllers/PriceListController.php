42e6f51589c08bdcf5d3ba7a2e546eeb8abc7e35
Владимир Лисевич <vovanhikkan@gmail.com>
2020-04-15 22:35:39 +0500

    MED-226

--- app/src/local/yii-app/modules/v1/controllers/PriceListController.php
+++ app/src/local/yii-app/modules/v1/controllers/PriceListController.php
@@ -35,16 +35,26 @@ class PriceListController extends Controller
         }
 
         \Bitrix\Main\Loader::IncludeModule('iblock');
-        $elRes = \CIBlockElement::GetList([], ['IBLOCK_CODE' => 'pricelist'], false, [], ["ID", "NAME", "PROPERTY_PRICE", "PROPERTY_CODE"]);
-
+        $elRes = \CIBlockElement::GetList([], ['IBLOCK_CODE' => 'pricelist'], false, [], ["IBLOCK_SECTION_ID", "ID", "NAME", "PROPERTY_PRICE", "PROPERTY_CODE"]);
+        $secRes = \CIBlockSection::GetList([], ['IBLOCK_CODE' => 'pricelist'], false, ['NAME', "ID"], false);
         $arFields = [];
+        $sections = [];
+        while($section = $secRes->GetNext())
+        {
+            $sections[$section['ID']] = [
+                'id' => $section['ID'],
+                'name' => $section['NAME']
+            ];
+        }
         while($ob = $elRes->GetNextElement())
         {
             $fields = $ob->GetFields();
+
             $arFields[$fields['ID']] = [
                 'name' => $fields["NAME"],
                 'code' => $fields["PROPERTY_CODE_VALUE"],
                 'price' => $fields["PROPERTY_PRICE_VALUE"],
+                'category' => $fields['IBLOCK_SECTION_ID']
             ];
         }
 
@@ -67,15 +77,31 @@ class PriceListController extends Controller
                 $data[] = [
                     'name' => $arRow['UF_TRANSLATE'],
                     'code' => $arFields[$arRow['UF_ELEM']]['code'],
-                    'price' => $arFields[$arRow['UF_ELEM']]['price']
+                    'price' => $arFields[$arRow['UF_ELEM']]['price'],
+                    'category' => $arFields['SECTION_ID']
                 ];
             }
 
+            $data = $this->prepare($data, $sections);
             $result->setSuccess($data);
             return $result->getJSON();
         }
 
+        $arFields = $this->prepare($arFields, $sections);
         $result->setSuccess($arFields);
         return $result->getJSON();
+    }
+
+    private function prepare($data = [], $sections = []) {
+        $res = [];
+        foreach ($data as $price) {
+            $res[$sections[$price['category']]['id']]['data'][] = [
+                'name' => $price['name'],
+                'code' => $price['code'],
+                'price' => $price['price'],
+            ];
+            $res[$sections[$price['category']]['id']]['name'] = $sections[$price['category']]['name'];
+        }
+        return $res;
     }
 }
