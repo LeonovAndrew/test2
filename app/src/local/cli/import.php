1908b7eae226d3d6bd6dfe9ed4e6084f6e7a4cfb
Владимир Лисевич <vovanhikkan@gmail.com>
2020-04-15 16:19:33 +0500

    ci/cd

--- app/src/local/cli/import.php
+++ app/src/local/cli/import.php
@@ -1,5 +1,5 @@
 <?php
-$_SERVER['DOCUMENT_ROOT'] = __DIR__ . '/../..';
+$_SERVER['DOCUMENT_ROOT'] = realpath(__DIR__ . '/../..');
 require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
 $importer = new ImportPriceList();
 $importer->import();
